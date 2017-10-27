a911af3e80900d23fd92c4782d81c3bbe37eed4e
Tiffany Hoang <thoang@squiz.net>
2017-10-27 16:25:49 +1100

    props,events, router

--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -1,11 +1,40 @@
 import React from 'react';
 
-class Header extends React.Component {
-    render() {
-        return (
-            <p>Header</p>
-        )
-    }
+// class Header extends React.Component {
+//     render() {
+//         return (
+//             <header className='top'>
+//                 <h1>
+//                     Catch 
+//                     <span className="ofThe">
+//                         <span className="of">of</span>
+//                         <span className="the">the</span>
+//                     </span>
+//                     day
+//                 </h1>
+//                 <h3 className="tagline"><span>{this.props.tagline}</span></h3>
+//             </header> 
+//         )
+//     }
+// }
+
+// stateless functional
+const Header = (props) => {
+    return (
+        <header className='top'>
+            <h1>
+                Catch 
+                <span className="ofThe">
+                    <span className="of">of</span>
+                    <span className="the">the</span>
+                </span>
+                day
+            </h1>
+            <h3 className="tagline"><span>{props.tagline}</span></h3>
+        </header> 
+    )
 }
+
+        
 
 export default Header;
---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { getFunName } from '../helpers';
+
+class StorePicker extends React.Component {
+    // constructor() {
+    //     super();
+    //     this.goToStore = this.goToStore.bind(this);
+    // }
+
+    goToStore(event) {
+        event.preventDefault();
+        console.log('You changed the URL');
+        // first grab the text fromt the box
+        console.log(this.storeInput.value)
+        const storeID = this.storeInput.value;
+        console.log(`Going to ${storeID}`)
+        // (this) above is not working. there are 2 way to get this work; 1 is constructor
+        // second got to transition from/ to /store/:storeid
+        this.context.router.transitionTo(`/store/${storeID}`)
+
+
+    }
+
+    render() {
+        return (
+            <form className='store-selector' onSubmit={(e) => this.goToStore(e)}>
+                <h2>Please Enter A Store</h2>
+                <input type='text' required placeholder='Store name' defaultValue={getFunName()} ref={(input) => {this.storeInput = input}}/>
+                <button type='submit'>Visit Store →</button>
+            </form>
+        )
+    }
+}
+
+StorePicker.contextTypes = {
+    router: React.PropTypes.object
+}
+export default StorePicker;
